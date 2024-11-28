679f51fdb766a7ffbbb9e142908397e70a9e276a
Muukii <muukii.app@gmail.com>
2024-11-28 21:39:15 +0900

    Init

---
+++ Sources/ButtonStyleModifier/ButtonStyleModifier.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+
+public struct ButtonState: Equatable {
+  /// available also `_styled_isPressed`
+  public let isPressed: Bool
+
+  public let isEnabled: Bool
+  /// available also `_styled_isProcessing`
+  public let isProcessing: Bool
+}
+
+/// A button style that applies a modifier to the button's label.
+///
+/// - Available Environment Values:
+/// - isPressed: ``\._styled_isPressed``
+/// - isProcessing: ``\._styled_isProcessing``
+public struct ButtonStyleModifier<Modified: View>: ButtonStyle {
+
+  @Environment(\.isEnabled) private var isEnabled
+
+  public var isProcessing: Bool = false
+
+  private let modify: (ButtonStyle.Configuration.Label, ButtonState) -> Modified
+
+  public init(
+    @ViewBuilder modify: @escaping (ButtonStyle.Configuration.Label, ButtonState) -> Modified
+  ) {
+    self.modify = modify
+  }
+
+  public init<Modifier: ViewModifier>(modifier: Modifier)
+  where Modified == ModifiedContent<ButtonStyle.Configuration.Label, Modifier> {
+    self.init(modify: { label, _ in
+      label.modifier(modifier)
+    })
+  }
+
+  public func makeBody(configuration: Configuration) -> some View {
+    /// iOS 14 can't use @Environment in ButtonStyle protocol.
+    modify(
+      configuration.label,
+      .init(isPressed: configuration.isPressed, isEnabled: isEnabled, isProcessing: isProcessing)
+    )
+    .environment(\._styled_isPressed, configuration.isPressed)
+    .environment(\._styled_isProcessing, isProcessing)
+    .contentShape(Rectangle())
+    .allowsHitTesting(isProcessing == false)
+  }
+
+  public func processing(_ isProcessing: Bool) -> Self {
+    var new = self
+    new.isProcessing = isProcessing
+    return new
+  }
+
+}
+
+extension EnvironmentValues {
+
+  @Entry public var _styled_isPressed: Bool = false
+
+  @Entry public var _styled_isProcessing: Bool = false
+}
